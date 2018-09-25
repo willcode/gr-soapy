555170fde89fbbd7f542a081c299cdcf854e7fd8
Sdoukos <sdoukos@csd.uoc.gr>
2018-09-25 16:35:41 +0300

    Implements full TX functionality.

--- include/soapy/sink.h
+++ include/soapy/sink.h
@@ -29,9 +29,19 @@ namespace gr {
   namespace soapy {
 
     /*!
-     * \brief <+description of block+>
+     * \brief Sink block implements SoapySDR functionality for TX
      * \ingroup soapy
      *
+     * The soapy sink block reads a stream and transmits the samples.
+     * The sink block also provides Soapy API calls for transmitter settings.
+     * Device is a string containing the driver and type name of the
+     * device the user wants to use according to the Soapy* module
+     * documentation.
+     * Make parameters are passed through the xml block.
+     * Antenna and clock source can be left empty and default values
+     * will be used.
+     * This block has a message port, which consumes PMT messages.
+     * For a description of the command syntax , see \ref cmd_handler.
      */
     class SOAPY_API sink : virtual public gr::sync_block
     {
@@ -45,9 +55,25 @@ namespace gr {
        * constructor is in a private implementation
        * class. soapy::sink::make is the public interface for
        * creating new instances.
+       * @param frequency center frequency in Hz
+       * @param gain amplification value in dB
+       * @param sampling_rate samples per second
+       * @param bandwidth the baseband filter width in Hz
+       * @param antenna name of an available antenna
+       * @param channel an available channel on the device
+       * @param dc_offset the relative correction (1.0 max)
+       * @param dc_offset_mode true for automatic correction
+       * @param frequency_correction the correction value in PPM
+       * @param iq_balance the relative correction (1.0 max)
+       * @param clock_source the name of clock source
+       * @param device the device driver and type
        */
-      static sptr make(float frequency, float gain, float samp_rate, float bandwidth,
-                       const std::string antenna, size_t channel, const std::string device);
+      static sptr make(float frequency, float gain, float sampling_rate,
+                       float bandwidth, const std::string antenna,
+                       size_t channel, gr_complexd dc_offset,
+                       bool dc_offset_mode, double frequency_correction,
+                       gr_complexd iq_balance, const std::string clock_source,
+                       const std::string device);
 
       /* Callbacks for source fields */
       virtual void set_gain(size_t channel, float gain) = 0;
--- lib/sink_impl.cc
+++ lib/sink_impl.cc
@@ -25,53 +25,97 @@
 #include <gnuradio/io_signature.h>
 #include "sink_impl.h"
 
+const pmt::pmt_t CMD_CHAN_KEY = pmt::mp ("chan");
+const pmt::pmt_t CMD_FREQ_KEY = pmt::mp ("freq");
+const pmt::pmt_t CMD_GAIN_KEY = pmt::mp ("gain");
+const pmt::pmt_t CMD_ANTENNA_KEY = pmt::mp ("antenna");
+const pmt::pmt_t CMD_RATE_KEY = pmt::mp ("samp_rate");
+const pmt::pmt_t CMD_BW_KEY = pmt::mp ("bw");
+
 namespace gr
 {
   namespace soapy
   {
 
     sink::sptr
-    sink::make (float frequency, float gain, float samp_rate, float bandwidth,
-                const std::string antenna, size_t channel,
-                const std::string device)
+    sink::make (float frequency, float gain, float sampling_rate,
+                float bandwidth, const std::string antenna, size_t channel,
+                gr_complexd dc_offset, bool dc_offset_auto_mode,
+                double freq_correction, gr_complexd iq_balance,
+                const std::string clock_source, const std::string device)
     {
       return gnuradio::get_initial_sptr (
-          new sink_impl (frequency, gain, samp_rate, bandwidth, antenna,
-                         channel, device));
+          new sink_impl (frequency, gain, sampling_rate, bandwidth, antenna,
+                         channel, dc_offset, dc_offset_auto_mode,
+                         freq_correction, iq_balance, clock_source, device));
     }
 
     /*
      * The private constructor
      */
-    sink_impl::sink_impl (float frequency, float gain, float samp_rate,
+    sink_impl::sink_impl (float frequency, float gain, float sampling_rate,
                           float bandwidth, const std::string antenna,
-                          size_t channel, const std::string device) :
+                          size_t channel, gr_complexd dc_offset,
+                          bool dc_offset_auto_mode, double freq_correction,
+                          gr_complexd iq_balance,
+                          const std::string clock_source,
+                          const std::string device) :
             gr::sync_block ("sink",
                             gr::io_signature::make (1, 1, sizeof(gr_complex)),
                             gr::io_signature::make (0, 0, 0)),
             d_mtu (0),
+            d_message_port (pmt::mp ("command")),
             d_frequency (frequency),
             d_gain (gain),
-            d_sampling_rate (samp_rate),
+            d_sampling_rate (sampling_rate),
             d_bandwidth (bandwidth),
             d_antenna (antenna),
-            d_channel (channel)
+            d_channel (channel),
+            d_dc_offset (dc_offset),
+            d_dc_offset_auto_mode (dc_offset_auto_mode),
+            d_frequency_correction (freq_correction),
+            d_iq_balance (iq_balance),
+            d_clock_source (clock_source)
     {
       makeDevice (device);
       set_frequency (d_channel, d_frequency);
-      set_gain (d_channel, d_gain);
+      set_gain_mode (d_channel, d_gain, d_gain_mode);
       set_sample_rate (d_channel, d_sampling_rate);
       set_bandwidth (d_channel, d_bandwidth);
       if (d_antenna.compare ("") != 0) {
         set_antenna (0, d_antenna);
       }
+      set_dc_offset (d_channel, d_dc_offset, d_dc_offset_auto_mode);
+      set_dc_offset_mode (d_channel, d_dc_offset_auto_mode);
+      set_frequency_correction (d_channel, d_frequency_correction);
+      set_iq_balance (d_channel, d_iq_balance);
+      set_iq_balance (d_channel, d_iq_balance);
+      if (d_clock_source.compare ("") != 0) {
+        set_clock_source (d_clock_source);
+      }
       d_stream = d_device->setupStream (SOAPY_SDR_TX, "CF32");
       d_device->activateStream (d_stream);
       d_mtu = d_device->getStreamMTU (d_stream);
       d_bufs.resize (1);
-      std::vector<std::string> antena = d_device->listAntennas (SOAPY_SDR_TX,
-                                                                d_channel);
-      std::cout << "Antenna: " << antenna << std::endl;
+
+      message_port_register_in (d_message_port);
+      set_msg_handler (d_message_port,
+                       boost::bind (&sink_impl::msg_handler_command, this, _1));
+
+      register_msg_cmd_handler (
+          CMD_FREQ_KEY,
+          boost::bind (&sink_impl::cmd_handler_frequency, this, _1, _2));
+      register_msg_cmd_handler (
+          CMD_GAIN_KEY,
+          boost::bind (&sink_impl::cmd_handler_gain, this, _1, _2));
+      register_msg_cmd_handler (
+          CMD_RATE_KEY,
+          boost::bind (&sink_impl::cmd_handler_samp_rate, this, _1, _2));
+      register_msg_cmd_handler (
+          CMD_BW_KEY, boost::bind (&sink_impl::cmd_handler_bw, this, _1, _2));
+      register_msg_cmd_handler (
+          CMD_ANTENNA_KEY,
+          boost::bind (&sink_impl::cmd_handler_antenna, this, _1, _2));
     }
 
     /*
@@ -80,6 +124,13 @@ namespace gr
     sink_impl::~sink_impl ()
     {
       unmakeDevice (d_device);
+    }
+
+    void
+    sink_impl::register_msg_cmd_handler (const pmt::pmt_t &cmd,
+                                         cmd_handler_t handler)
+    {
+      d_cmd_handlers[cmd] = handler;
     }
 
     int
@@ -121,6 +172,17 @@ namespace gr
     }
 
     void
+    sink_impl::set_gain_mode (size_t channel, float gain,
+                                bool dc_offset_auto_mode)
+    {
+      /* If user specifies no automatic gain setting set gain value */
+      if (!dc_offset_auto_mode) {
+        d_device->setGain (SOAPY_SDR_TX, channel, gain);
+      }
+      d_device->setGainMode (SOAPY_SDR_TX, channel, dc_offset_auto_mode);
+    }
+
+    void
     sink_impl::set_sample_rate (size_t channel, float sample_rate)
     {
       d_device->setSampleRate (SOAPY_SDR_TX, channel, sample_rate);
@@ -136,6 +198,172 @@ namespace gr
     sink_impl::set_antenna (const size_t channel, const std::string &name)
     {
       d_device->setAntenna (SOAPY_SDR_TX, channel, name);
+    }
+
+    void
+    sink_impl::set_dc_offset (size_t channel, gr_complexd dc_offset,
+                              bool dc_offset_auto_mode)
+    {
+      /* If DC Correction is supported but automatic mode is not set DC correction */
+      if (!dc_offset_auto_mode
+          && d_device->hasDCOffset (SOAPY_SDR_TX, channel)) {
+        d_device->setDCOffset (SOAPY_SDR_TX, channel, dc_offset);
+        d_dc_offset = dc_offset;
+      }
+    }
+
+    void
+    sink_impl::set_dc_offset_mode (size_t channel, bool dc_offset_auto_mode)
+    {
+      /* If user specifies automatic DC Correction and is supported activate it */
+      if (dc_offset_auto_mode
+          && d_device->hasDCOffsetMode (SOAPY_SDR_TX, channel)) {
+        d_device->setDCOffsetMode (SOAPY_SDR_TX, channel, dc_offset_auto_mode);
+        d_dc_offset_auto_mode = dc_offset_auto_mode;
+      }
+    }
+
+    void
+    sink_impl::set_frequency_correction (size_t channel, double freq_correction)
+    {
+      /* If the device supports Frequency correction set value */
+      if (d_device->hasFrequencyCorrection (SOAPY_SDR_TX, channel)) {
+        d_device->setFrequencyCorrection (SOAPY_SDR_TX, channel,
+                                          freq_correction);
+        d_frequency_correction = freq_correction;
+      }
+    }
+
+    void
+    sink_impl::set_iq_balance (size_t channel, gr_complexd iq_balance)
+    {
+      /* If the device supports IQ blance correction set value */
+      if (d_device->hasIQBalance (SOAPY_SDR_TX, channel)) {
+        d_device->setIQBalance (SOAPY_SDR_TX, channel, iq_balance);
+        d_iq_balance = iq_balance;
+      }
+    }
+
+    void
+    sink_impl::set_master_clock_rate (double clock_rate)
+    {
+      d_device->setMasterClockRate (clock_rate);
+      d_clock_rate = clock_rate;
+    }
+
+    void
+    sink_impl::set_clock_source (const std::string &clock_source)
+    {
+      d_device->setClockSource (clock_source);
+      d_clock_source = clock_source;
+    }
+
+    void
+    sink_impl::set_frontend_mapping (const std::string &mapping)
+    {
+      d_device->setFrontendMapping (SOAPY_SDR_TX, mapping);
+    }
+
+    double
+    sink_impl::get_frequency (size_t channel)
+    {
+      return d_device->getFrequency (SOAPY_SDR_TX, channel);
+    }
+
+    double
+    sink_impl::get_gain (size_t channel)
+    {
+      return d_device->getGain (SOAPY_SDR_TX, channel);
+    }
+
+    bool
+    sink_impl::get_gain_mode (size_t channel)
+    {
+      return d_device->getGainMode (SOAPY_SDR_TX, channel);
+    }
+
+    double
+    sink_impl::get_sampling_rate (size_t channel)
+    {
+      return d_device->getSampleRate (SOAPY_SDR_TX, channel);
+    }
+
+    double
+    sink_impl::get_bandwidth (size_t channel)
+    {
+      return d_device->getBandwidth (SOAPY_SDR_TX, channel);
+    }
+
+    std::string
+    sink_impl::get_antenna (size_t channel)
+    {
+      return d_device->getAntenna (SOAPY_SDR_TX, channel);
+    }
+
+    std::complex<double>
+    sink_impl::get_dc_offset (size_t channel)
+    {
+      return d_device->getDCOffset (SOAPY_SDR_TX, channel);
+    }
+
+    bool
+    sink_impl::get_dc_offset_mode (size_t channel)
+    {
+      return d_device->getDCOffsetMode (SOAPY_SDR_TX, channel);
+    }
+
+    double
+    sink_impl::get_frequency_correction (size_t channel)
+    {
+      return d_device->getFrequencyCorrection (SOAPY_SDR_TX, channel);
+    }
+
+    std::complex<double>
+    sink_impl::get_iq_balance (size_t channel)
+    {
+      return d_device->getIQBalance (SOAPY_SDR_TX, channel);
+    }
+
+    double
+    sink_impl::get_master_clock_rate ()
+    {
+      return d_device->getMasterClockRate ();
+    }
+
+    std::string
+    sink_impl::get_clock_source ()
+    {
+      return d_device->getClockSource ();
+    }
+
+    void
+    sink_impl::cmd_handler_frequency (pmt::pmt_t val, size_t chann)
+    {
+      set_frequency (chann, pmt::to_float (val));
+    }
+
+    void
+    sink_impl::cmd_handler_gain (pmt::pmt_t val, size_t chann)
+    {
+      set_gain (chann, pmt::to_float (val));
+    }
+
+    void
+    sink_impl::cmd_handler_samp_rate (pmt::pmt_t val, size_t chann)
+    {
+      set_sample_rate (chann, pmt::to_float (val));
+    }
+
+    void
+    sink_impl::cmd_handler_bw (pmt::pmt_t val, size_t chann)
+    {
+      set_bandwidth (chann, pmt::to_float (val));
+    }
+
+    void
+    sink_impl::cmd_handler_antenna (pmt::pmt_t val, size_t chann)
+    {
+      set_antenna (chann, pmt::symbol_to_string (val));
     }
 
     int
@@ -163,6 +391,23 @@ namespace gr
       return noutput_items;
     }
 
+    void
+    sink_impl::msg_handler_command (pmt::pmt_t msg)
+    {
+      if (!pmt::is_dict (msg)) {
+        return;
+      }
+      size_t chann = 0;
+      if (pmt::dict_has_key (msg, CMD_CHAN_KEY)) {
+        chann = pmt::to_long (
+            pmt::dict_ref (msg, CMD_CHAN_KEY, pmt::from_long (0)));
+        pmt::dict_delete (msg, CMD_CHAN_KEY);
+      }
+      for (size_t i = 0; i < pmt::length (msg); i++) {
+        d_cmd_handlers[pmt::car (pmt::nth (i, msg))] (
+            pmt::cdr (pmt::nth (i, msg)), chann);
+      }
+    }
   } /* namespace soapy */
 } /* namespace gr */
 
